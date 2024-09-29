801029e200ab096fb9e53611a4c7dd0ef35250a4
FioriMartins <caiofiorigamer1234@outlook.com>
2024-09-29 00:38:14 -0300

    0.0.2 - Adição do sistema de vidas

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const alfabeto = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","t","s","u","v","w","x","y","z"]
+const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "t", "s", "u", "v", "w", "x", "y", "z"]
 const palavras = ["tiago", "jose"]
 const palavraEscolhida = palavras[Math.floor(Math.random() * palavras.length)]
 const arrayPalavra = []
+var vidasGastas = 4
 
 document.addEventListener('DOMContentLoaded', palavraTratamento())
 
@@ -9,23 +10,32 @@ alfabeto.forEach((letra) => {
     document.querySelector('.botaos').innerHTML += `<div class="button" onclick="letra('${letra}')">${letra}</div>`
 })
 
-function palavraTratamento () {
-    for (let i = 0; i < palavraEscolhida.length; i++) {
-        arrayPalavra.push('_')
-    } 
-    document.querySelector('.texto').innerHTML += arrayPalavra.join('')
-
-    const palavraUsu = document.querySelector('.texto').innerHTML
-    console.log(arrayPalavra.join(''))
+for (let i = 1; i <= 3; i++) {
+    document.querySelector('.vida').innerHTML += `<img src="assets/coração.png" id="heart${i}">`
 }
 
-function letra (letter) {
-    let libidgel = letter
+function palavraTratamento() {
+    for (let i = 0; i < palavraEscolhida.length; i++) {
+        arrayPalavra.push('_')
+    }
+    document.querySelector('.texto').innerHTML += arrayPalavra.join('')
+}
+
+function letra(letter) {
+    let i = 0
 
     arrayPalavra.forEach((letr, index) => {
         if (letter == palavraEscolhida.charAt(index)) {
             arrayPalavra[index] = letter
-        } 
+        } else {
+            i++
+        }
+        
+        if (i === palavraEscolhida.length) {
+            vidasGastas--
+
+            document.querySelector('#heart'+vidasGastas).classList.add('morto')
+        }
     })
 
     document.querySelector('.texto').innerHTML = arrayPalavra.join('')
@@ -33,10 +43,13 @@ function letra (letter) {
     ganhou()
 }
 
-function ganhou () {
-    arrayPalavra.forEach((letr, index) => {
-        if (arrayPalavra.join('') == palavraEscolhida) {
-            alert('ganhou, vagabunda')
-        }
-    })
+function ganhou() {
+    if (arrayPalavra.join('') == palavraEscolhida) {
+        alert('ganhou, vagabunda')
+        location.reload(true)
+    } 
+
+    if (vidasGastas == 1) {
+        alert('perdeu mamon')
+    }
 }
