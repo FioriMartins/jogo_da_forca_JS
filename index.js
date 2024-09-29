4b850f48a06d35e7eba96d964a2e54c714c60b4d
FioriMartins <caiofiorigamer1234@outlook.com>
2024-09-29 02:08:09 -0300

    0.0.3 - Adição de estilo e sistema novo de charadas

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
-const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "t", "s", "u", "v", "w", "x", "y", "z"]
-const palavras = ["tiago", "jose"]
-const palavraEscolhida = palavras[Math.floor(Math.random() * palavras.length)]
+const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "T", "S", "U", "V", "W", "X", "Y", "Z"]
+const palavras = [
+    {
+        "resposta": "JORNAL",
+        "desc": "Eu tenho páginas, mas não sou livro. Você me lê para aprender sobre o que acontece no mundo."
+    },
+    {
+        "resposta": "ELEVADOR",
+        "desc": "Eu estou sempre subindo e descendo, mas nunca saio do lugar. O que sou?"
+    },
+    {
+        "resposta": "TECLADO",
+        "desc": "Eu tenho teclas, mas não sou piano. Eu tenho letras, mas não sou livro."
+    },
+    {
+        "resposta": "PRIMO",
+        "desc": "Eu sou um número que não pode ser dividido por mais nada além de mim mesmo e 1."
+    },
+    {
+        "resposta": "VENTO",
+        "desc": "Eu corro sem pernas e assobio sem boca. O que sou?"
+    },
+    {
+        "resposta": "ESPONJA",
+        "desc": "Eu sou cheio de buracos, mas seguro a água."
+    },
+    {
+        "resposta": "LUA",
+        "desc": "Eu sou redondo e brilhante, mas não sou o sol. Apareço à noite e mudo de forma."
+    },
+    {
+        "resposta": "AVIAO",
+        "desc": "Eu sou pequeno, leve, e viajo em grandes alturas. No entanto, posso derrubar algo muito maior que eu."
+    }
+]
+const randomIndex = Math.floor(Math.random() * palavras.length)
+const palavraEscolhida = palavras[randomIndex].resposta
 const arrayPalavra = []
 var vidasGastas = 4
 
 document.addEventListener('DOMContentLoaded', palavraTratamento())
 
 alfabeto.forEach((letra) => {
-    document.querySelector('.botaos').innerHTML += `<div class="button" onclick="letra('${letra}')">${letra}</div>`
+    document.querySelector('.botaos').innerHTML += `<div id="button" name="${letra}" onclick="letra('${letra}')">${letra}</div>`
 })
 
 for (let i = 1; i <= 3; i++) {
@@ -19,6 +53,7 @@ function palavraTratamento() {
         arrayPalavra.push('_')
     }
     document.querySelector('.texto').innerHTML += arrayPalavra.join('')
+    document.querySelector('.charada').innerHTML = palavras[randomIndex].desc
 }
 
 function letra(letter) {
@@ -30,11 +65,11 @@ function letra(letter) {
         } else {
             i++
         }
-        
+
         if (i === palavraEscolhida.length) {
             vidasGastas--
 
-            document.querySelector('#heart'+vidasGastas).classList.add('morto')
+            document.querySelector('#heart' + vidasGastas).classList.add('morto')
         }
     })
 
@@ -47,9 +82,10 @@ function ganhou() {
     if (arrayPalavra.join('') == palavraEscolhida) {
         alert('ganhou, vagabunda')
         location.reload(true)
-    } 
+    }
 
     if (vidasGastas == 1) {
         alert('perdeu mamon')
+        location.reload(true)
     }
 }
